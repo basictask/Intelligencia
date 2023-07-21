c3c5476e2b36a834b41f5863d095124d68c13a9f
Daniel Kuknyo <49187483+basictask@users.noreply.github.com>
2023-07-21 15:55:31 +0200

    Added some files for solving_3

--- render.py
+++ render.py
@@ -12,11 +12,14 @@ target_dir = os.path.dirname(os.path.realpath(__file__))
 for root, _, files in os.walk(target_dir):
     for file in files:
         file_path = os.path.join(root, file)
+        file_dir = os.path.abspath(file_path)
+        file_name = os.path.splitext(os.path.basename(file_path))[0] + '.png'
+        target_name = os.path.join(file_dir, file_name)
+	
         if file_path.endswith('.dot'):
             print('Rendering: ' + file_path + '... ', end = '') 
             (graph, ) = pydot.graph_from_dot_file(file_path)
-            name = file_path.split('.')[0]
-            graph.write_png(os.path.join(root, name) + '.png')
+            graph.write_png(file_dir)
             print('Done')
 
 print('Done')
