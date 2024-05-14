948d415db39ed81a8733c2f90a466f8727ec47d0
basictask <daniel.kuknyo@mailbox.org>
2024-05-14 21:22:19 +0200

    scipts update and README update

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
-# Intelligencia
-Üzleti Intelligencia - Mesterséges intelligencia alkalmazásai üzleti kontextusban: BGE kurzus
+# Budapesti Gazdasági Egyetem
+## Üzleti Intelligencia
+Mesterséges intelligencia alkalmazásai üzleti kontextusban. 
 
 ## Feldolgozott témák a kurzus alatt
-~~~
+
 - Bevezetés
-1. Verziókezelés
+1. Verziókezelés: `1_git_dok`
 - Megerősítéses tanulás
-2. Bevezetés a megerősítéses tanulásba
-3. Markov döntési folyamatok megoldása
-4. Sztochasztikus becslés
-5. Q-tanulás
-6. Megerősítéses mélytanulás
+2. Bevezetés a megerősítéses tanulásba: `2_reinforcement_learning`
+3. Markov döntési folyamatok megoldása: `3_solving_mdp`
+4. Sztochasztikus becslés: `4_mc_td`
+5. Q-tanulás: `5_ql`
+6. Megerősítéses mélytanulás: `6_dql`
 - Mélytanulás
-7. Bevezetés a mesterséges mélytanulásba
-8. Objektum detekció
-9. Egyed szegmentáicó
-10. Visszacsatolásos neurális hálózatok
-11. Transzformáló archtiketkúrák
-~~~	
+7. Bevezetés a mesterséges mélytanulásba: `7_dl`
+8. Objektum detekció: `8_od`
+9. Egyed szegmentáicó: `9_instance`
+10. Visszacsatolásos neurális hálózatok: `10_recurrent`
+11. Transzformáló archtiketkúrák: `11_transformer`
+
+## Mappák struktúrája
+A gyökérmappában található scriptek:  
+- `merge_pdfs.py`: Konkatenálja az összes pdf-et egy dokumentummá a gyökérmappába.  
+- `render_graph.py`: Renderel minden dot gráfot az összes mappában.  
+- `render_pdf.py`: Renderel minden LaTeX fájlt pdf formátumba.  
+    
+Minden mappában két almappa található:  
+- `code`: Esettanulány, ami megvalósítja a tanult témakört.  
+- `doc`: Dokumentáció LaTeX és pdf formátumban.  
+	- `images`: Diagramok és képek a dokumentációban. Itt megtalálható egy témának megfelelően elnevezeett Jupyter notebook, ami a képek generálásáért felelős. Az itt található programkód publikus és szabadon felhasználható az órai munka során.  
+	- `graphs` (opcionális): Gráfok dot formátumban, amit a Graphviz szoftver segítségével lehet lerenderelni.   
--- merge_pdfs.py
+++ merge_pdfs.py
@@ -1,51 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Small script to merge all class file pdfs into a single large pdf
+"""
+Small script to merge all class file pdfs into a single large pdf
+"""
 
 import os
 import re
+import sys
 import fitz
 
-root_folder = '.'
-output_filename = 'intelligencia_merged.pdf'
+if __name__ == '__main__':
+    # Main loop
+    root_folder = os.path.dirname(os.path.abspath(__file__))  # By default the root folder is the folder of the script
+    output_filename = 'elemzesmodszertan_merged.pdf'
 
-if os.path.isfile(output_filename):
-    os.remove(output_filename)
-    print(f'Removed {output_filename}')
+    if os.path.isfile(output_filename):
+        os.remove(output_filename)
+        print(f'Removed {output_filename}\n')
 
-# Regular expression to match directories in {number}_{text}/doc format
-dir_pattern = re.compile(r'^\./(\d{1,2})_([^/]+)/doc/(\d+)_(.+)\.pdf$')
+    # Regular expression to match directories in {number}_{text}/doc format
+    dir_pattern = re.compile(r'.*(\d+)_(.*)\.pdf$')
 
-# List to store (number, path) tuples
-numbered_paths = []
+    # List to store (number, path) tuples
+    numbered_paths = []
 
-# Walk through the directory
-for root, dirs, files in os.walk(root_folder):
-    for file in files:
-        # Full path of the file
-        full_path = os.path.join(root, file)
-        match = dir_pattern.match(full_path)
-        if match:
-            numbered_paths.append((int(match.group(1)), full_path))
-            print(full_path)
+    # Walk through the directory
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+            # Full path of the file
+            full_path = os.path.abspath(os.path.join(root, file))
+            match = dir_pattern.match(full_path)
+            if match:
+                numbered_paths.append((int(match.group(1)), full_path))
+                print(full_path)
 
-# Sort the directories based on the number
-numbered_paths.sort()
+    # Sort the directories based on the number
+    sort_pattern = re.compile(r'\d+')
+    numbered_paths = sorted(numbered_paths, key=lambda x: int(sort_pattern.search(os.path.basename(x[1])).group()))
 
-print('Paths to merge:\n', numbered_paths)
-print('Running merge on paths...')
+    # Check if there are paths matched and exit if no
+    if len(numbered_paths) == 0:
+        print('No documents found to merge.')
+        sys.exit()
 
-merged_document = fitz.open()
+    print('\nPaths to merge:')
+    for path in numbered_paths:
+        print(path)
 
-# Merge PDFs from each directory
-for _, full_path in numbered_paths:
-    with fitz.open(full_path) as pdf:
-        for page in range(len(pdf)):
-            merged_document.insert_pdf(pdf, from_page=page, to_page=page)
+    print('\nRunning merge on paths...', end='')
 
-merged_document.save(output_filename)
-merged_document.close()
+    merged_document = fitz.open()
 
-print('Output successful')
+    # Merge PDFs from each directory
+    for _, full_path in numbered_paths:
+        with fitz.open(full_path) as pdf:
+            for page in range(len(pdf)):
+                merged_document.insert_pdf(pdf, from_page=page, to_page=page)
+        print('*', end='')
 
+    merged_document.save(output_filename)
+    merged_document.close()
+
+    print('\nOutput successful')
